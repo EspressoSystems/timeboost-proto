35727fd72ce699d7c96b1d97f16a43c52a8de002
Luke Iannucci <luke.iannucci@outlook.com>
2025-11-19 16:42:15 -0500

    add rust gens

---
+++ rust/protos/src/main.rs
@@ -0,0 +1,19 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    tonic_prost_build::configure()
+        .build_server(true)
+        .out_dir("src/generated")
+        .bytes(".block.Block.payload")
+        .bytes(".inclusion.Transaction.encoded_txn")
+        .compile_protos(
+            &[
+                "../../inclusion_list.proto",
+                "../../block.proto",
+                "../../internal.proto",
+                "../../forward.proto",
+            ],
+            &["../../"],
+        )?;
+    Ok(())
+}
